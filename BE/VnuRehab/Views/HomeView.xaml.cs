2ae0cac90bb5f81b1f0aacbb37b1eb1118b57d7f
TienDat <multivncraft@gmail.com>
2025-04-30 17:08:42 +0700

    update home view

--- BE/VnuRehab/Views/HomeView.xaml.cs
+++ BE/VnuRehab/Views/HomeView.xaml.cs
@@ -14,23 +14,8 @@ namespace VnuRehab.Views
             _viewModel = viewModel;
             DataContext = _viewModel;
 
-            // Set up event handlers for the toggle buttons
-            UiPrmdToggle.Checked += UiPrmdToggle_Checked;
-            KimoreToggle.Checked += KimoreToggle_Checked;
         }
 
-        private void UiPrmdToggle_Checked(object sender, RoutedEventArgs e)
-        {
-            // Show UI-PRMD exercises and hide KIMORE exercises
-            UiPrmdExercises.Visibility = Visibility.Visible;
-            KimoreExercises.Visibility = Visibility.Collapsed;
-        }
-
-        private void KimoreToggle_Checked(object sender, RoutedEventArgs e)
-        {
-            // Show KIMORE exercises and hide UI-PRMD exercises
-            UiPrmdExercises.Visibility = Visibility.Collapsed;
-            KimoreExercises.Visibility = Visibility.Visible;
-        }
+     
     }
 }
